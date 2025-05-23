8e4139e297ffab560656ff28ecaa8e403235358d
Juan Garrido Liébana <juangarridoliebana@gmail.com>
2025-05-23 22:09:14 +0200

    Subida inicial: script de documentación

---
+++ practica-Garrido.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Suma dos números enteros.
+ *
+ * @param int $a Primer sumando.
+ * @param int $b Segundo sumando.
+ * @return int La suma de ambos números.
+ * @author Garrido
+ * @version 1.0
+ * @internal Esta función es solo para uso interno.
+ */
+function suma($a, $b) {
+    return $a + $b;
+}
+
+/**
+ * Multiplica dos números.
+ *
+ * @param float $x Primer factor.
+ * @param float $y Segundo factor.
+ * @return float El producto de ambos números.
+ * @author Garrido
+ * @version 1.0
+ */
+function multiplica($x, $y) {
+    return $x * $y;
+}
